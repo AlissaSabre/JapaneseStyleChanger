f0dddb11df2f9297b18e3215f82f46a29076fde5
Alissa Sabre <alissa_sabre@yahoo.co.jp>
2020-08-16 18:05:40 +0900

    Refactoring of HtmlHandler: more comments and a minor aesthetic change.

--- JapaneseStyleChanger/HtmlHandler.cs
+++ JapaneseStyleChanger/HtmlHandler.cs
@@ -89,6 +89,7 @@ namespace JapaneseStyleChanger
                             {
                                 // the minimum length of a valid character entity reference is 4 (e.g., &#9; or &lt;),
                                 // so this is invalid.
+                                // this case also covers no semicolon was found.
                             }
                             else if (html[q + 1] != '#')
                             {
@@ -128,8 +129,11 @@ namespace JapaneseStyleChanger
                         {
                             // Grab a tag.
                             var r = html.IndexOf('>', q + 1);
-                            if (r < q + 2) // the minimum length of a valid tag is 3, e.g., <i>.
+                            if (r < q + 2)
                             {
+                                // the minimum length of a valid tag is 3, e.g., <i>,
+                                // so this is invalid.
+                                // this case also covers no '>' was found.
                                 sb.Append('<');
                                 q = q + 1;
                             }
@@ -158,10 +162,21 @@ namespace JapaneseStyleChanger
 
         public string GetUpdatedHtml()
         {
-            var tags = Tags; int tindex = 0;
+            var tags = Tags;
+            int tindex = 0;
+
             var text = UpdatedText;
 
             var sb = new StringBuilder(text.Length + tags.Count * 8);
+
+            // By the following loop, we merge subtexts in text (= UpdatedText)
+            // and tags in tags (= Tags) appropriately, considering the positions of
+            // each tag in the original html text AND the correspondence between the
+            // text and original html via nodes in UpdatedNodes.  The result will be
+            // in sb.
+
+            // To do so, we examine node in UpdatedNodes one by one in the following
+            // loop.
 
             int p = 0;
             int qpos, bpos, epos = 0;
@@ -170,10 +185,11 @@ namespace JapaneseStyleChanger
                 int q = text.IndexOfIgnoreWidth(node.Surface, p);
                 if (q < 0)
                 {
-                    // This case is unexpected,
-                    // though we can safely keep going by simply ignoring it.
+                    // This case is unexpected, but if it happened,
+                    // we just ignore the node to keep going.
                     continue;
                 }
+
                 if (node.IsOriginalNode())
                 {
                     // BPos points to the beginning of any preceding whitespaces
@@ -192,7 +208,7 @@ namespace JapaneseStyleChanger
                 }
                 else
                 {
-                    // The node u is not from the original node list.
+                    // The node was not from the original node list.
                     // We use the most passable estimation
                     // for the purpose of tag placement in the case.
                     qpos = epos;
@@ -200,27 +216,54 @@ namespace JapaneseStyleChanger
                     // epos = epos;
                 }
 
+                // At this moment, we assume that the subtexts in text that correspond
+                // to any node before the current node have been put in sb, being merged with 
+                // tags[0] to tags[tindex - 1] already.
+                // We are about to put the subtext that corresponds to the current node
+                // into sb.
+                // If there are any tags that were before the node in the original
+                // HTML text (i.e., Tag.Pos < bpos), put them in sb before the text that
+                // corresponds to the current node.
+                // IF there were some whitespaces between the previous node and the current node,
+                // place each of those tags in an appropriate position in the whitespace block.
+                // We use two values original_preceding_padding and original_succeeding_padding
+                // to find the best positions of tags; the original_preceding_padding is the number
+                // of whitespace characters after the previous node and before the tag,
+                // and the original_succeeding_padding is the number of whitespace characters 
+                // after the tag and before the current node, both counted in the original HTML text.
+                // Remember, bpos points to the beginning of such whitespace block and qpos points
+                // to the end of it.
+                // We try to more respect original_preceding_padding than original_succeeding_padding
+                // if it is a close tag, so that the extra spaces added to the updated text
+                // go outside of elements as much as possible.
+                // We assume texts between text[p] and text[q - 1], inclusive, are all whitespace
+                // (or anything that are not important), and distribute them in between 
+                // the tags appropriately.
+                // the variable r points in a middle of p and q, where text[p] to text[r - 1]
+                // are already put into sb, and text[r] to text[q - 1] are not (i.e., ready for
+                // further consumption.)
+
                 int r = p;
                 while (tindex < tags.Count && tags[tindex].Pos <= qpos && tags[tindex].IsCloseTag)
                 {
                     int original_preceding_padding = tags[tindex].Pos - bpos;
                     if (original_preceding_padding > r - p)
                     {
-                        sb.AppendEscaped(text.Substring(r, Math.Min(q - r, original_preceding_padding)));
-                        r += Math.Min(q - r, original_preceding_padding);
+                        var n = Math.Min(q - r, original_preceding_padding);
+                        sb.AppendEscaped(text.Substring(r, n));
+                        r += n;
                     }
                     sb.Append(tags[tindex].TagText);
                     tindex++;
                 }
-                //sb.AppendEscaped(utext.Substring(p, q - p));
-                //p = q;
                 while (tindex < tags.Count && tags[tindex].Pos <= qpos)
                 {
                     int original_succeeding_padding = qpos - tags[tindex].Pos;
                     if (original_succeeding_padding < q - r)
                     {
-                        sb.AppendEscaped(text.Substring(r, q - r - original_succeeding_padding));
-                        r = q - original_succeeding_padding;
+                        var n = q - r - original_succeeding_padding;
+                        sb.AppendEscaped(text.Substring(r, n));
+                        r += n;
                     }
                     sb.Append(tags[tindex].TagText);
                     tindex++;
@@ -232,6 +275,8 @@ namespace JapaneseStyleChanger
                 sb.AppendEscaped(text.Substring(q, node.Length));
                 p = q + node.Length;
             }
+
+            // handle any remaining tags and texts.
             {
                 int q = text.Length;
                 int r = p;
@@ -253,8 +298,19 @@ namespace JapaneseStyleChanger
         }
     }
 
+    /// <summary>
+    /// Provides some extension methods to be used by the internals of HtmlHandler.
+    /// </summary>
+    /// <remarks>
+    /// This class is marked public only because the C# language grammar requires it.
+    /// </remarks>
     public static class HtmlHandlerHelperMethods
     {
+        /// <summary>
+        /// Appends a text, after escaping characters in it as HTML character references where needed.
+        /// </summary>
+        /// <param name="sb">A <see cref="StringBuilder"/> instance.</param>
+        /// <param name="text">The (unescaped) string to be appended.</param>
         public static void AppendEscaped(this StringBuilder sb, string text)
         {
             int p = 0;
@@ -287,6 +343,26 @@ namespace JapaneseStyleChanger
             }
         }
 
+        /// <summary>
+        /// Reports the index of the specified string,
+        /// starting the search at the specified index, 
+        /// and matching the (sub)strings in a width-insensitve mannar.
+        /// </summary>
+        /// <param name="s">The string to search <paramref name="text"/> in.</param>
+        /// <param name="text">The string to find.</param>
+        /// <param name="index">The index in <paramref name="s"/> to start searching at.</param>
+        /// <returns>
+        /// The first index in the whole <paramref name="s"/> after <paramref name="index"/> where <paramref name="text"/> is at,
+        /// or a negative value if no matching string was found after <paramref name="index"/>.
+        /// </returns>
+        /// <remarks>
+        /// For the purpose of this function,
+        /// <i>width-insensive</i> means that an ASCII character (i.e., U+0021 thru U+007E)
+        /// is considered equivalent to its FULLWIDTH version (i.e., those in U+FF01 thru U+FF5E)
+        /// as well as ASCII SPACE (U+0020) is to IDEOGRAPHIC SPACE (U+3000) (aka fullwidth space).
+        /// We never consider other special equivalence, 
+        /// especially for other FULLWIDTH and HALFWIDTH forms in U+FF5E thru U+FFEE. 
+        /// </remarks>
         public static int IndexOfIgnoreWidth(this string s, string text, int index)
         {
             const int FullwidthShift = 0xFEE0;
@@ -326,6 +402,10 @@ namespace JapaneseStyleChanger
         /// <returns></returns>
         public static bool IsOriginalNode(this WNode node)
         {
+            // Because EPos is a base-zero index, and there is no
+            // token with length zero, MeCab tokenizer never assigns a 0 to EPos,
+            // while JapaneseStyleChanger.Conjugator never assigns a value to it,
+            // leaving its default value, i.e., 0.
             return node.EPos > 0;
         }
     }
